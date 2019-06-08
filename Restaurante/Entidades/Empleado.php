c33d3c64bbb00df4be10dfeb6a3d91455f7fef76
ayeV <ayelenvaldez07@gmail.com>
2019-06-08 18:07:24 -0300

    start

--- Restaurante/Entidades/Empleado.php
+++ Restaurante/Entidades/Empleado.php
@@ -13,7 +13,42 @@
 
 
     public static function Registrar($usuario, $clave,$nombre,$tipo){
-        
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $respuesta = "";
+        try {
+            date_default_timezone_set("America/Argentina/Buenos_Aires");
+            $fecha = date('Y-m-d H:i:s');
+
+            $consulta = $objetoAccesoDato->RetornarConsulta("SELECT ID_tipo_empleado FROM tipoempleado WHERE descripcion = :tipo AND estado = 'A';");
+
+            $consulta->bindValue(':tipo', $tipo, PDO::PARAM_STR);
+            $consulta->execute();
+            $id_tipo = $consulta->fetch();
+
+            if ($id_tipo != null) {
+                $consulta = $objetoAccesoDato->RetornarConsulta("INSERT INTO empleados (ID_tipo_empleado, nombre_empleado, usuario, 
+                clave, fecha_registro, estado) 
+                VALUES (:id_tipo, :nombre, :usuario, :clave, :fecha, 'A');");
+
+                $consulta->bindValue(':nombre', $nombre, PDO::PARAM_STR);
+                $consulta->bindValue(':usuario', $usuario, PDO::PARAM_STR);
+                $consulta->bindValue(':clave', $clave, PDO::PARAM_STR);
+                $consulta->bindValue(':fecha', $fecha, PDO::PARAM_STR);
+                $consulta->bindValue(':id_tipo', $id_tipo[0], PDO::PARAM_INT);
+
+                $consulta->execute();
+
+                $respuesta = array("Estado" => "OK", "Mensaje" => "Empleado registrado correctamente.");
+            } else {
+                $respuesta = array("Estado" => "ERROR", "Mensaje" => "Debe ingresar un tipo de empleado valido");
+            }
+        } catch (Exception $e) {
+            $mensaje = $e->getMessage();
+            $respuesta = array("Estado" => "ERROR", "Mensaje" => "$mensaje");
+        }
+        finally {
+            return $respuesta;
+        }
 
     }
 
